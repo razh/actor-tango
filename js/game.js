36d473f0f164b3ccdad8fd754d91cfbc04791c80
Raymond Zhou <somecrazyidiot@gmail.com>
2014-01-24 19:14:44 -0500

    Basic player movement.

--- js/game.js
+++ js/game.js
@@ -1,18 +1,46 @@
 (function( window, document, undefined ) {
   'use strict';
 
+  function Entity( x, y ) {
+    this.x = x;
+    this.y = y;
+
+    this.image = null;
+  }
+
+  function Player( x, y ) {
+    Entity.call( this, x, y );
+  }
+
+  Player.prototype.draw = function( ctx ) {
+    ctx.beginPath();
+    ctx.rect( this.x - 10, this.y - 10, 20, 20 );
+    ctx.fillStyle = 'red';
+    ctx.fill();
+  };
+
+  Player.prototype.update = function( dt ) {
+    var dx = dt * 100;
+    var dy = dt * 100;
+
+    if ( game.input.keys[ 37 ] ) { this.x -= dx; }
+    if ( game.input.keys[ 39 ] ) { this.x += dx; }
+    if ( game.input.keys[ 38 ] ) { this.y -= dy; }
+    if ( game.input.keys[ 40 ] ) { this.y += dy; }
+  };
+
   function Game() {
     this.prevTime = Date.now();
     this.currTime = this.prevTime;
     this.running = true;
 
-    var canvas = document.createElement( 'canvas' );
-    var ctx = canvas.getContext( '2d' );
+    this.canvas = document.createElement( 'canvas' );
+    this.ctx = this.canvas.getContext( '2d' );
 
-    canvas.width = 512;
-    canvas.height = 512;
+    this.canvas.width = 512;
+    this.canvas.height = 512;
 
-    document.body.appendChild( canvas );
+    document.body.appendChild( this.canvas );
 
     this.entities = [];
 
@@ -25,15 +53,19 @@
         down: false
       }
     };
+
+    this.entities.push( new Player( 256, 256 ) );
   }
 
   Game.prototype.draw = function() {
     var ctx = this.ctx;
 
-    var width = ctx.width,
-        height = ctx.height;
+    var width = ctx.canvas.width,
+        height = ctx.canvas.height;
 
     ctx.clearRect( 0, 0, width, height );
+    ctx.fillStyle = 'white';
+    ctx.fillRect( 0, 0, width, height );
 
     this.entities.forEach(function( entity ) {
       entity.draw( ctx );
@@ -65,7 +97,12 @@
 
 
   function onKeyDown( event ) {
+    console.log(event.which)
+    game.input.keys[ event.which ] = true;
+  }
 
+  function onKeyUp( event ) {
+    game.input.keys[ event.which ] = false;
   }
 
   function tick() {
@@ -75,8 +112,11 @@
 
     game.update();
     game.draw();
-    window.requstAnimationFrame( tick );
+    window.requestAnimationFrame( tick );
   }
 
   tick();
+
+  document.addEventListener( 'keydown', onKeyDown );
+  document.addEventListener( 'keyup', onKeyUp );
 }) ( window, document );
